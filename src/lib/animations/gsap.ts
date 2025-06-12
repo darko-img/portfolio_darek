681480c756b414520dc76ac0d14ed4306dcc0279
Dariusz Tomaszewski <darekimg@gmail.com>
2025-06-12 15:28:30 +0200

    observer done 4

--- src/lib/animations/gsap.ts
+++ src/lib/animations/gsap.ts
@@ -93,14 +93,14 @@ export const observeReveal = (
   tl.fromTo(
     selectors.work,
     { clipPath: "polygon(0 100%, 100% 100%, 100% 100%, 0 100%)", },
-    { clipPath: "polygon(0 0%, 100% 0%, 100% 100%, 0 100%)", duration: 1.25, ease: defaultEase, force3D: true, },
+    { clipPath: "polygon(0 0%, 100% 0%, 100% 100%, 0 100%)", duration: 1.3, ease: defaultEase, force3D: true, },
     "<0.25"
   );
 
   tl.fromTo(
     selectors.workSection,
     { yPercent: -30, },
-    { yPercent: 0, duration: 1.25, ease: defaultEase, force3D: true },
+    { yPercent: 0, duration: 1.3, ease: defaultEase, force3D: true },
     "<"
   );
 
