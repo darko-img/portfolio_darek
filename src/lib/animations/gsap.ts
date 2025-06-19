91ff8417cd86382d9b5275ed46e581cd96ebbc8e
Dariusz Tomaszewski <darekimg@gmail.com>
2025-06-19 12:31:44 +0200

    info block mobile 29

--- src/lib/animations/gsap.ts
+++ src/lib/animations/gsap.ts
@@ -47,20 +47,20 @@ export const aboutReveal = (
   tl.fromTo(
     selectors.about,
     { yPercent: 150, },
-    { yPercent: 0, duration: 1.0, stagger: 0.1, ease: defaultEase, force3D: true }
+    { yPercent: 0, duration: 1.5, stagger: 0.1, ease: defaultEase, force3D: true }
   );
 
   tl.fromTo(
     selectors.portrait,
     { clipPath: "polygon(0 100%, 100% 100%, 100% 100%, 0 100%)", },
-    { clipPath: "polygon(0 0%, 100% 0%, 100% 100%, 0 100%)", duration: 1.5, stagger: 0.1, ease: defaultEase, force3D: true },
+    { clipPath: "polygon(0 0%, 100% 0%, 100% 100%, 0 100%)", duration: 1.25, ease: defaultEase, force3D: true },
     "<0.35"
   );
 
   tl.fromTo(
     selectors.tReveal,
     { opacity: 0 },
-    { opacity: 1, duration: 0.25, ease: defaultEase, force3D: true },
+    { opacity: 1, duration: 0.1, ease: defaultEase, force3D: true },
     "<0.3"
   );
   return tl;
